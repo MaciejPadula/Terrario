48a304c4445f4afb20f754e5df87c9e70720415d
Maciej Padula <maciej.padula@wp.pl>
2026-02-03 11:35:54 +0100

    add analytics

--- src/terrario.client/src/shared/hooks/useFirebase.ts
+++ src/terrario.client/src/shared/hooks/useFirebase.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
+import { getAnalytics } from "firebase/analytics";
 import { config } from "../../config";
 
 export function useFirebaseApp() {
   try {
     const app = initializeApp(config.firebaseConfig);
     const messaging = getMessaging(app);
-    return { app, messaging };
+    const analytics = getAnalytics(app);
+    return { app, messaging, analytics };
   } catch {
-    return { app: null, messaging: null };
+    return { app: null, messaging: null, analytics: null };
   }
 }
