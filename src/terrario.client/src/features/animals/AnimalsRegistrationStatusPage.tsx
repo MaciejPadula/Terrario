67b34d7ec2e8c0c7ce0d02fda88b45864b64cbd7
Maciej Padula <maciej.padula@wp.pl>
2026-03-16 17:02:36 +0100

    fix

--- src/terrario.client/src/features/animals/AnimalsRegistrationStatusPage.tsx
+++ src/terrario.client/src/features/animals/AnimalsRegistrationStatusPage.tsx
@@ -1,11 +1,8 @@
 import { Box, VStack } from "@chakra-ui/react";
-import { useTranslation } from "react-i18next";
 import { AnimalsTabs } from "./components/AnimalsTabs";
 import { RegistrationStatusTable } from "./components/RegistrationStatusTable";
 
 export function AnimalsRegistrationStatusPage() {
-  const { t } = useTranslation();
-
   return (
     <VStack align="stretch" gap={6}>
       <AnimalsTabs />
